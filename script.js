b5984cddea6a9cef092e88d2b37405a90463b175
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-26 15:21:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to log the animal's sound
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
+
+// Define the Cat class that extends Animal
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Method for the cat to purr
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Define the Dog class that extends Animal
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Method for the dog to bark
+  bark() {
+    console.log("woof");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
